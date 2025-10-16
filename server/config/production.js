1103de1c20594946620b0a4080ff2d4a1a8f5ad2
alex <alex101226@outlook.com>
2025-10-16 13:56:08 -0700

    先这样吧

--- server/config/production.js
+++ server/config/production.js
@@ -1,13 +1,14 @@
 //  生产环境
 export default {
-  port: 3000,
+
+  port: 9091,
   host: '0.0.0.0',
   db: {
     port: 3306,
-    user: 'znrycl',
-    password: 'Root@2025', //  Root@2025
-    host: '127.0.0.1',
-    database: 'znrycl',
+    user: 'vehicle',
+    password: 'Root@2025', //  Root@2025  Root@2025
+    host: 'vehicle-db',
+    database: 'vehicle_db',
   },
   jwt: {
     secret: 'o2v9WqV3hM8u7zYfHcPqR1s5lTgBj9DkXxN4d0K2SxA=',
